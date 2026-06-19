028a289b19b53cb69e19c93944c04e4e570755c6
PacketAlchemy <packetalchemy@gmail.com>
2026-06-19 11:51:01 +0330

    Update vless.js

--- src/protocols/vless.js
+++ src/protocols/vless.js
@@ -1,1 +1,13 @@
-
+export async function handleVless() {
+  return new Response(
+    JSON.stringify({
+      protocol: "vless",
+      status: "not implemented"
+    }),
+    {
+      headers: {
+        "content-type": "application/json"
+      }
+    }
+  );
+}
