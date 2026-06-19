e616ed19cfe68a0976522862dfc3aa77e3999739
PacketAlchemy <packetalchemy@gmail.com>
2026-06-19 11:50:40 +0330

    Update index.js

--- src/protocols/index.js
+++ src/protocols/index.js
@@ -1,1 +1,15 @@
+import { handleVless } from "./vless";
+import { handleTrojan } from "./trojan";
 
+export async function dispatchProtocol(type, request) {
+  switch (type) {
+    case "vless":
+      return handleVless(request);
+
+    case "trojan":
+      return handleTrojan(request);
+
+    default:
+      return null;
+  }
+}
